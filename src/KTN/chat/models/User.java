930223227dc8858c442c59f7f2b5c7616cc7a0cd
Khanhke2210 <khanh.le.set18@eiu.edu.vn>
2023-01-14 09:27:51 +0700

    User Model
    Finishing User Model

--- src/KTN/chat/models/User.java
+++ src/KTN/chat/models/User.java
@@ -3,7 +3,7 @@ package KTN.chat.models;
 import java.sql.Date;
 
 public class User {
-	private String fisrtName;
+	private String firstName;
 	private String lastName;
 	private String hashPassword;
 	private String gender;
@@ -12,9 +12,54 @@ public class User {
 	private User(String firstName, String lastName, 
 			String hashPassword, String gender, 
 			Date dateOfBirth) {
-		this.fisrtName = firstName;
+		this.firstName = firstName;
 		this.lastName = lastName;
 		this.hashPassword = hashPassword;
 		this.gender = gender;
 	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getFullName() {
+		String fullName = firstName + " " + lastName;
+		return fullName;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public boolean verifyPasword(String inputPasword) {
+		return hashPassword.equalsIgnoreCase(inputPasword);
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+    public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+    
+    public void setGender(String gender) {
+    	this.gender = gender;
+    }
+    
+    public void setDateOfBirth(Date dateOfBirth) {
+    	this.dateOfBirth = dateOfBirth;
+    }
+    
+    public void setHashPassword(String hashPassword) {
+    	this.hashPassword = hashPassword;
+    }
 }
