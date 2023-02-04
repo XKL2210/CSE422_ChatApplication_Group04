77dca4ae54d923f49a5d464a2cec91f7174d131a
Khanhke2210 <khanh.le.set18@eiu.edu.vn>
2023-02-04 10:13:16 +0700

    Data Storage

---
+++ src/ktn/chat/repositories/Repository.java
@@ -0,0 +1,40 @@
+package ktn.chat.repositories;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Repository<Entity> implements IRepository<Entity>{
+	List<Entity> list = new ArrayList<>();
+
+	@Override
+	public List<Entity> get(Predicate<Entity> predicate, Comparator<Entity> filter) {
+		List<Entity> result;
+        result = list.stream().filter(predicate).sorted(filter).collect(Collectors.toList());
+        return result;
+	}
+
+	@Override
+	public Entity find(Predicate<Entity> predicate) {
+		return list.stream().filter(predicate).findFirst().orElse(null);
+	}
+
+	@Override
+	public void insert(Entity entity) {
+		list.add(entity);
+	}
+
+	@Override
+	public void update(Entity entity, Entity entity1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Entity entity) {
+		list.remove(entity);
+	}
+	
+}
