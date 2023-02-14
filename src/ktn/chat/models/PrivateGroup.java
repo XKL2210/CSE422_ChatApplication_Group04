c1e47b982d5ca04a39f6bbda8c26b4cc50b7fcf9
Khanhke2210 <khanh.le.set18@eiu.edu.vn>
2023-02-14 10:09:05 +0700

    PrivateGroup Modify

--- src/ktn/chat/models/PrivateGroup.java
+++ src/ktn/chat/models/PrivateGroup.java
@@ -2,10 +2,21 @@ package ktn.chat.models;
 
 import java.util.ArrayList;
 
+import ktn.chat.enums.GroupMemberRole;
+
 public class PrivateGroup extends Group{
+	private String joinPasscode;
 
 	public PrivateGroup(String id, User founder, ArrayList<User> users) {
 		super(id, founder, users);
 		
 	}
+	
+	public String getJoinPasscode() {
+        return joinPasscode;
+    }
+
+    public void setJoinPasscode(String joinPasscode) {
+        this.joinPasscode = joinPasscode;
+    }
 }
