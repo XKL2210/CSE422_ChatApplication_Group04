baaf196bc90f57da1c42c3a15c080b1e259199eb
Khanhke2210 <khanh.le.set18@eiu.edu.vn>
2023-02-14 10:05:24 +0700

    User Modify

--- src/ktn/chat/models/User.java
+++ src/ktn/chat/models/User.java
@@ -3,7 +3,7 @@ package ktn.chat.models;
 import java.sql.Date;
 
 public class User {
-	private String fisrtName;
+	private String firstName;
 	private String lastName;
 	private String hashPassword;
 	private String gender;
@@ -17,4 +17,50 @@ public class User {
 		this.hashPassword = hashPassword;
 		this.gender = gender;
 	}
+	
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getFullName() {
+		String fullName = firstName + " " + lastName;
+		return fullName;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public boolean verifyPasword(String inputPasword) {
+		return hashPassword.equalsIgnoreCase(inputPasword);
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+    public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+    
+    public void setGender(String gender) {
+    	this.gender = gender;
+    }
+    
+    public void setDateOfBirth(Date dateOfBirth) {
+    	this.dateOfBirth = dateOfBirth;
+    }
+    
+    public void setHashPassword(String hashPassword) {
+    	this.hashPassword = hashPassword;
+    }
 }
